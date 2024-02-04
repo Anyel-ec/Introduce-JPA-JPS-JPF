69b986283131b97d689e10d2dfc2e88fc1a93ef0
anyel-ec <paulangel826@gmail.com>
2024-02-04 09:36:40 -0500

    add data access object

---
+++ src/main/java/com/espe/idao/IUsuarioDAO.java
@@ -0,0 +1,12 @@
+package com.espe.idao;
+import com.espe.model.Usuario;
+
+import java.util.List;
+
+public interface IUsuarioDAO {
+    void guardar (Usuario usuario);
+    void editar (Usuario usuario);
+    Usuario buscarUsuario (int id);
+    List<Usuario> listar();
+    // desarrollado por Anyel EC
+}
