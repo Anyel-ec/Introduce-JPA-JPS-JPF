c2eb729d8056e5cc2b34d2c7caacb1cdc3f72b4a
anyel-ec <paulangel826@gmail.com>
2024-02-04 09:11:55 -0500

    add model of user and JPAUtil, drivers to mysql, and edit persistence.xml

---
+++ src/main/java/com/espe/model/JPAUtil.java
@@ -0,0 +1,23 @@
+package com.espe.model;
+
+import jakarta.persistence.EntityManagerFactory;
+
+public class JPAUtil {
+    // Nombre de la unidad de persistencia
+    private static final String PERSISTENCE_UNIT_NAME = "PERSISTENCE_UNIT";
+    private static EntityManagerFactory factory; //para crear los gestores de entidades
+    // para obtener la factoria de entidades
+    public static EntityManagerFactory getEntityManagerFactory() {
+        if (factory == null) { // si no existe la factoria de entidades
+            // se crea la factoria de entidades
+            factory = jakarta.persistence.Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
+        }
+        return factory; // se retorna la factoria de entidades
+    }
+    // desarrollado por Anyel EC
+    public static void shutdown() { // para cerrar la factoria de entidades
+        if (factory != null) { // si existe la factoria de entidades
+            factory.close(); // se cierra la factoria de entidades
+        }
+    }
+}
---
+++ src/main/java/com/espe/model/Usuario.java
@@ -0,0 +1,80 @@
+package com.espe.model;
+
+import jakarta.persistence.*;
+
+// Tabla de usuario
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+    // Atributos de la tabla usuario
+    @Id
+    // Generar el id de manera automatica
+    @GeneratedValue (strategy= GenerationType.IDENTITY)
+    private Integer id;
+    private String nombre;
+    private String apellido;
+    private String cedula;
+    private String ciudad;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer id, String nombre, String apellido, String cedula, String ciudad) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.ciudad = ciudad;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCiudad() {
+        return ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        this.ciudad = ciudad;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", cedula='" + cedula + '\'' +
+                ", ciudad='" + ciudad + '\'' +
+                '}';
+    }
+}
